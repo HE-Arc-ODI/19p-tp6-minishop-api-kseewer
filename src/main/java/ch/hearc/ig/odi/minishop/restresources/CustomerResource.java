03172da068016199878886b9162ebbaa13f2afcd
kseewer <40600885+kseewer@users.noreply.github.com>
2019-03-25 21:07:33 +0100

    implémentation du code CustomerResource avec erreur 404

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -4,7 +4,55 @@
 
 package ch.hearc.ig.odi.minishop.restresources;
 
+import ch.hearc.ig.odi.minishop.business.Customer;
+import ch.hearc.ig.odi.minishop.business.Order;
+import ch.hearc.ig.odi.minishop.exception.CustomerException;
+import ch.hearc.ig.odi.minishop.exception.NotFoundException;
+import ch.hearc.ig.odi.minishop.exception.OrderException;
+import ch.hearc.ig.odi.minishop.services.PersistenceService;
+import java.text.ParseException;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("customer")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+
+
 public class CustomerResource {
 
-}
+    @Inject
+    private PersistenceService persistenceService;
+
+    @GET
+    public List<Customer> customerGet() {
+      return persistenceService.getAllCustomers();
+    }
+
+    @GET
+    @Path("{id}")
+    public Customer getCustomerId(@PathParam("id") Long customerId) {
+      try {
+        return persistenceService.getCustomerByID(customerId);
+      } catch (CustomerException e) {
+        e.printStackTrace();
+        throw new NotFoundException("the customer does not exist");
+      }
+    }
+
+    @POST
+    public Order customerPost(@FormParam("customerId") Long customerId) throws ParseException, OrderException {
+      return persistenceService.createOrder(customerId);
+    }
+  }
+
 
