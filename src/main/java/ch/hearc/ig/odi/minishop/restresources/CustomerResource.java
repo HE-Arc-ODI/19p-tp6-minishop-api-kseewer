c16aab560a99bd0e270ce06c1e0e835d8281c100
kseewer <40600885+kseewer@users.noreply.github.com>
2019-04-01 17:03:13 +0200

    ajout delete avec exception

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -13,6 +13,7 @@ import java.text.ParseException;
 import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -30,29 +31,31 @@ import javax.ws.rs.core.MediaType;
 
 public class CustomerResource {
 
-    @Inject
-    private PersistenceService persistenceService;
+  @Inject
+  private PersistenceService persistenceService;
 
-    @GET
-    public List<Customer> customerGet() {
-      return persistenceService.getAllCustomers();
-    }
+  @GET
+  public List<Customer> customerGet() {
+    return persistenceService.getAllCustomers();
+  }
 
-    @GET
-    @Path("{id}")
-    public Customer getCustomerId(@PathParam("id") Long customerId) {
-      try {
-        return persistenceService.getCustomerByID(customerId);
-      } catch (CustomerException e) {
-        e.printStackTrace();
-        throw new NotFoundException("the customer does not exist");
-      }
+  @GET
+  @Path("{id}")
+  public Customer getCustomerId(@PathParam("id") Long customerId) {
+    try {
+      return persistenceService.getCustomerByID(customerId);
+    } catch (CustomerException e) {
+      e.printStackTrace();
+      throw new NotFoundException("the customer does not exist");
     }
+  }
 
-    @POST
-    public Customer customerPost(@FormParam("username") String username,@FormParam("firstName") String firstName,@FormParam("lastName") String lastName,@FormParam("email") String email, @FormParam("phone") String phone  ){
-      return persistenceService.createAndPersistCustomer(username,firstName,lastName,email,phone);
-    }
+  @POST
+  public Customer customerPost(@FormParam("username") String username,
+      @FormParam("firstName") String firstName, @FormParam("lastName") String lastName,
+      @FormParam("email") String email, @FormParam("phone") String phone) {
+    return persistenceService.createAndPersistCustomer(username, firstName, lastName, email, phone);
+  }
 
   @PUT
   @Path("{id}")
@@ -66,6 +69,16 @@ public class CustomerResource {
     }
   }
 
+  @DELETE
+  @Path("{id}")
+  public void deleteCustomer(@PathParam("id") Long id) throws CustomerException {
+    try {
+      persistenceService.deleteCustomer(id);
+    } catch (CustomerException e) {
+      e.printStackTrace();
+      throw new CustomerException("customer could not be deleted");
+    }
   }
+}
 
 
